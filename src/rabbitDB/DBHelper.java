bf7c29ffe44c85fe1dc67a9c551ea16e97b607c3
정현진 <s2113@e-mirim.hs.kr>
2023-04-03 00:45:33 +0900

    DB와 테이블이 없을시, 생성하는 코드

---
+++ src/rabbitDB/DBHelper.java
@@ -0,0 +1,35 @@
+package rabbitDB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBHelper {
+    public DBHelper() {
+        String url = "jdbc:mysql://127.0.0.1/?useSSL=false&user=root&password=@summer0573";
+        String userName = "root";
+        String password = "@summer0573";
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("드라이버 연결 성공!");
+            Connection connection = DriverManager.getConnection(url, userName, password);
+            Statement stmt = connection.createStatement();
+            String createSql = "CREATE DATABASE IF NOT EXISTS `rabbitScoreDB`";  // DB가 없다면 DB를 만들어라
+
+            stmt.executeUpdate(createSql);
+            url="jdbc:mysql://localhost/AdaventureOfWhaleDB?serverTimezone=UTC";
+            connection = DriverManager.getConnection(url, userName, password);
+            stmt = connection.createStatement();
+            createSql = "CREATE TABLE IF NOT EXISTS rabbit_table(name varchar(20)," +
+                    " first_score int, first_clear  tinyint(1))" +
+                    "second_score int, second_clear tinyint(1)";  //tableㅇㅣ 없다면 table를 만들어라
+
+            stmt.executeUpdate(createSql);
+            stmt.close();
+            connection.close();
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.print(e);
+        }
+    }
+}
